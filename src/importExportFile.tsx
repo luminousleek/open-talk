8015358aa2beda912ae02d5df07600254aab6f4a
luminousleek <68138671+luminousleek@users.noreply.github.com>
2021-08-30 11:54:24 +0800

    Put sample cards in accordions

--- src/importExportFile.tsx
+++ src/importExportFile.tsx
@@ -2,6 +2,8 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
 import { CardTuple, QuestionCard, DisplayCard } from './PlayingCard';
 import { CardDeck } from "./decks";
 
@@ -27,37 +29,88 @@ const openSampleCard = new QuestionCard("Names", "Launch", "What does your name 
 openSampleCard.uncover();
 const closedSampleCard = new QuestionCard("Names", "Launch", "What does your name mean?", "Self");
 closedSampleCard.disableButton();
+const openMissingCard = new QuestionCard("", "\u200b", "What does your name mean?", "Self");
+openMissingCard.uncover();
+const closedMissingCard = new QuestionCard("", "\u200b", "What does your name mean?", "Self");
+closedMissingCard.disableButton();
 
 export const importText:JSX.Element = (
     <>
         <p>Question Cards in open talk are made up 4 components - Question Title, Depth Level, Question and Category. These components are displayed in the following manner:</p>
-        <Row className="justify-content-center">
-            <Col md="auto">
-                {DisplayCard(openCard, () => {})}
-                <p>Front of card</p>
-            </Col>
-            <Col md="auto">
-                {DisplayCard(closedCard, () => {})}
-                <p>Back of card</p>
-            </Col>
-        </Row>
+        <Accordion>
+            <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="0">
+                    Question Card Format (click to expand)
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                        <Row className="justify-content-center">
+                            <Col md="auto">
+                                {DisplayCard(openCard, () => {})}
+                                <p>Front of card</p>
+                            </Col>
+                            <Col md="auto">
+                                {DisplayCard(closedCard, () => {})}
+                                <p>Back of card</p>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+        </Accordion>
+        <br />
         <p>To import questions, each of these components must be separated by the <a href="https://en.wikipedia.org/wiki/Vertical_bar" target="_blank" rel="noreferrer">vertical bar (|)</a> character
                 in this order: Question Title|Depth Level|Question|Category</p>
         <p>For example, for a Question where the Question Title is "Names", the Depth Level is "Launch", the Question is "What does your name mean?" and the Category is "Self", 
             it will be typed as</p> 
         <p>Names|Launch|What does your name mean?|Self</p>
         <p>and displayed as follows:</p>
-        <Row className="justify-content-center">
-            <Col md="auto">
-                {DisplayCard(openSampleCard, () => {})}
-                <p>Front of card</p>
-            </Col>
-            <Col md="auto">
-                {DisplayCard(closedSampleCard, () => {})}
-                <p>Back of card</p>
-            </Col>
-        </Row>
-        <p>Question Titles and Depth Levels are optional, but the vertical pipe character still needs to be put in. E.g. ||What does your name mean?|Self will import a Question Card without a Title and Depth Level.</p>
+        <Accordion>
+            <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="0">
+                    Sample Card Format (click to expand)
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                        <Row className="justify-content-center">
+                            <Col md="auto">
+                                {DisplayCard(openSampleCard, () => {})}
+                                <p>Front of card</p>
+                            </Col>
+                            <Col md="auto">
+                                {DisplayCard(closedSampleCard, () => {})}
+                                <p>Back of card</p>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+        </Accordion>
+        <br />
+        <p>Question Titles and Depth Levels are optional, but the vertical pipe character still needs to be put in.</p>
+        <p>For example, "||What does your name mean?|Self" will import a Question Card without a Title and Depth Level, and will be displayed as follows:</p>
+        <Accordion>
+            <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="0">
+                    Card with Missing Question Title and Depth Level (click to expand)
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                        <Row className="justify-content-center">
+                            <Col md="auto">
+                                {DisplayCard(openMissingCard, () => {})}
+                                <p>Front of card</p>
+                            </Col>
+                            <Col md="auto">
+                                {DisplayCard(closedMissingCard, () => {})}
+                                <p>Back of card</p>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+        </Accordion>
+        <br />
         <p>Take note that each Question Card must be put on its own line, and the maximum number of Categories is 10.</p>
         <p>It is recommended to type out the Question Cards in a separate program before copying and pasting it in.</p>
     </>
